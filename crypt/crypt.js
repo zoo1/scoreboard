3d6b1d8b5aa5239b1541c75d7c65f6a0c5dce60e
Zachary Oscar Ovington <zoo1@pitt.edu>
2015-02-21 17:05:04 -0500

    crypto changes

--- crypt/crypt.js
+++ crypt/crypt.js
@@ -1,10 +1,8 @@
 // Nodejs encryption with CTR
 var crypto = require('crypto'),
-algorithm = 'aes-256-ctr',
-password = 'd6F3Efeq';
 
 module.exports.decrypt = function (text) {
-var decipher = crypto.createDecipher('aes-256-ctr','d6F3Efeq');
+var decipher = crypto.createDecipher('aes-256-cbc','test');
 var dec = decipher.update(text,'plaintext','utf8');
 dec += decipher.final('utf8');
 return dec;
